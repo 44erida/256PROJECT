afce0564e623686225e14ff6307f614f4428ffe9
Alejandro Maduro <alejandro200761@gmail.com>
2026-04-14 18:09:39 +0300

    Main index.ejs page template

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get("/", (req, res) => {
-   res.render("index.ejs")
+    res.render("index.ejs")
 })
 
 app.get("/seller", (req, res) => {
-   res.render("main-seller.ejs")
+    res.render("main-seller.ejs")
 })
 
-app.listen(3000,()=>{
-    console.log("HIII")
+app.listen(3000, () => {
+    console.log("Server running on port 3000")
 })
 
