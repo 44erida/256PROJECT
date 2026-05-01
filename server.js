d337a8fc752d523bf7834e68de2706e795263bfb
Alejandro <93278101+Waduro@users.noreply.github.com>
2026-05-01 22:29:11 +0300

    user-register endpoint added

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ import db from "./db.js";
 const app = express();
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false, 
-  saveUninitialized: false
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
 }));
 
 app.get("/", (req, res) => {
@@ -29,7 +29,7 @@ app.get("/seller/products", (req, res) => {
 
 app.get("/logout", (req, res) => {
     req.session.destroy(err => {
-        if (err) {  
+        if (err) {
             return res.redirect("/dashboard")
         }
         res.clearCookie("connect.sid")
@@ -53,30 +53,34 @@ app.get("/register", (req, res) => {
     res.render("register");
 });
 
+app.get("/user-register", async (req, res) => {
+    res.render("user-register")
+})
+
 app.post("/login", async (req, res) => {
     //login system for customer and buyers
     const { email, password, remember } = req.body
     try {
         const [rows] = await db.query(
-             "SELECT * FROM user WHERE email = ?", [email])
-         if (rows.length > 0) {
-             const user = rows[0]
-             const match = await bcrypt.compare(password, user.password)
-             if (match) {
-                 req.session.user = user
-                 req.session.isAuthenticated = true
-                 if (remember) {
-                     req.session.cookie.maxAge = 30 * 24 * 60 * 60 * 1000
-                 }  
-                res.render("seller-home", {user: req.session.user})
-             } else {
-                 req.session.message = "Invalid username or password"
-                 res.redirect("/login")
-             }
-         } else {
-             req.session.message = "Invalid username or password" 
-             res.redirect("/login")
-         }
+            "SELECT * FROM user WHERE email = ?", [email])
+        if (rows.length > 0) {
+            const user = rows[0]
+            const match = await bcrypt.compare(password, user.password)
+            if (match) {
+                req.session.user = user
+                req.session.isAuthenticated = true
+                if (remember) {
+                    req.session.cookie.maxAge = 30 * 24 * 60 * 60 * 1000
+                }
+                res.render("seller-home", { user: req.session.user })
+            } else {
+                req.session.message = "Invalid username or password"
+                res.redirect("/login")
+            }
+        } else {
+            req.session.message = "Invalid username or password"
+            res.redirect("/login")
+        }
     } catch (error) {
         res.status(500).send(error.code)
     }
@@ -91,9 +95,9 @@ app.post("/register", async (req, res) => {
         req.session.message = "Registration successful. Please login.";
         res.redirect("/login");
     } catch (error) {
-        
+
     }
-    
+
 })
 
 app.listen(3000, () => {
